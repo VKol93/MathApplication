6f93d4bb65f54f72c3a5cb0e85f823cd276dafff
vk <valeriia.koltsova@gmail.com>
2020-08-30 09:41:03 -0400

    08/30/2020

--- app/src/main/java/com/vk/mathaplication/FragmentYear.kt
+++ app/src/main/java/com/vk/mathaplication/FragmentYear.kt
@@ -1,14 +1,17 @@
 package com.vk.mathaplication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.data_fragment.*
 import kotlinx.android.synthetic.main.data_fragment.okButton
 import kotlinx.android.synthetic.main.data_fragment.yearEditText
 import kotlinx.android.synthetic.main.year_fragment.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FragmentYear : Fragment() {
 
@@ -23,10 +26,23 @@ class FragmentYear : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         okButton.setOnClickListener {
             val year = yearEditText.text.toString()
+            remoteService.getYearFact(year.toInt()).enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    yearoutputtextView.text = response.body()
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    yearoutputtextView.text = "Server error bye"
+                    Log.d("mylog",t.message)
+                }
+
+            })
+
         }
         clearButton.setOnClickListener {
             yearEditText.text.clear()
         }
+
 
     }
 
